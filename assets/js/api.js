10d5339b82b4cdb4a3948a4b95bf81fe96a2c811
Acolyte03 <Acolyte03@gmail.com>
2023-10-08 18:49:31 -0500

    Added our API key and API URL along with our credentials

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,3 +3,12 @@
     // fetch (url) {
     // method, cache, credentials, etc, if needed    
     //} .then (response)
+
+    // const url = 'https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup';
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': 'a4648e0474msh710bc51684197e7p129ccajsn9a00cae65197',
+    //         'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+    //     }
+    // };
