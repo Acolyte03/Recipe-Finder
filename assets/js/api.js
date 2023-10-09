fadfad61579cc088421abbe0d7f5ccc0a8ba8d4a
Acolyte03 <Acolyte03@gmail.com>
2023-10-09 13:33:41 -0500

    Added drink API and fixed API key publicity

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,20 +5,22 @@
     //} .then (response)
 
     
-    var url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=6&q=';
-    var options = {
+    var foodUrl = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=6&q=';
+    var foodOptions = 
+    {
         method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'a4648e0474msh710bc51684197e7p129ccajsn9a00cae65197',
+        headers: 
+        {
+            'X-RapidAPI-Key': config.api_key_1,
             'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
         }
     };
-       function apiSearch(event)
+       function apiFoodSearch(event)
     {
         event.preventDefault();
-        var query = document.querySelector("#query");
-        var search = url.concat(query.value);
-        fetch(search, options).then(function (response) 
+        var foodQuery = document.querySelector("#query");
+        var foodSearch = foodUrl.concat(foodQuery.value);
+        fetch(foodSearch, foodOptions).then(function (response) 
         {
             return response.json()
         })
@@ -30,11 +32,49 @@
                 
             // }
             console.log(response);
-            console.log(search);
+            console.log(foodSearch);
         })
         .catch(function(error)
         {
             console.error(error);
         });
     } 
-     inputButton.addEventListener('click', apiSearch);
+     inputButton.addEventListener('click', apiFoodSearch);
+
+    var drinkUrl = 'https://the-cocktail-db.p.rapidapi.com/search.php?s=';
+    var drinkOptions = 
+    {
+        method: 'GET',
+        headers: 
+        {
+            'X-RapidAPI-Key': 'config.api_key_2',
+            'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+        }
+    };
+
+
+    function apiDrinkSearch(event)
+    {
+        event.preventDefault();
+        var drinkQuery = document.querySelector("#query");
+        var drinkSearch = drinkUrl.concat(drinkQuery.value);
+        fetch(drinkSearch, drinkOptions).then(function (response) 
+        {
+            return response.json()
+        })
+        .then(function(response)
+        {
+            // //build cards here
+            // for(i = 0; i < 6; i++)
+            // {
+                
+            // }
+            console.log(response);
+            console.log(drinkSearch);
+        })
+        .catch(function(error)
+        {
+            console.error(error);
+        });
+    } 
+    inputButton.addEventListener('click', apiDrinkSearch);
