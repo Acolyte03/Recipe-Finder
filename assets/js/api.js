a263404528c47781a378ab20dca3d756bf02c1f4
Acolyte03 <Acolyte03@gmail.com>
2023-10-09 10:46:28 -0500

    API is correctly being called without throwing errors

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,11 +4,24 @@
     // method, cache, credentials, etc, if needed    
     //} .then (response)
 
-    // const url = 'https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup';
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'X-RapidAPI-Key': 'a4648e0474msh710bc51684197e7p129ccajsn9a00cae65197',
-    //         'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    //     }
-    // };
+    var url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=6&tags=under_30_minutes';
+    var options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'a4648e0474msh710bc51684197e7p129ccajsn9a00cae65197',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+    fetch(url, options).then(function (response) 
+    {
+        return response.json()
+    })
+    .then(function(response)
+    {
+        //build cards here
+        console.log(response);
+    })
+    .catch(function(error)
+    {
+        console.error(error);
+    });
